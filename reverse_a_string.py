7cb81fa1973da3ffffb18844baa6b4fca112e344
suhailmajeed-pixel <suhailmajeed@gmail.com>
2026-01-30 18:38:05 +0530

    Add files via upload

---
+++ reverse_a_string.py
@@ -0,0 +1,8 @@
+string = input("please enter your string :")
+string2 = ("")
+
+for i in string :
+    string2 = i+string2
+
+print ("/nthe original string=", string)
+print("the revrese string =",string2)
